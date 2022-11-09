d72a146f6cd4c0aa91af361e33c29ff27bfab554
Taosif Shaad <mir.md.taosif.nur@g.bracu.ac.bd>
2022-11-09 13:28:10 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 use Softzino\Autoloadpractice\Animal\{Tiger, Cat as WildCat};
 use Softzino\Autoloadpractice\Pet\Cat;
+use function softzino\autoloadpractice\Helper\sum;
 
 require_once 'vendor/autoload.php';
 
 echo (new Tiger(true))->getStatus() . PHP_EOL;
 echo (new Cat("Purr")) . PHP_EOL;
 echo (new WildCat()) . PHP_EOL;
+
+echo sum(10, 20);
